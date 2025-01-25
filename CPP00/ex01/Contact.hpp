58ac14a58659283d5cbe97ba917cf7b1a18e6ccb
paul <akapaui@protonmail.com>
2025-01-25 23:22:18 +0100

    Contact.hpp done

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -10,3 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+class Contact
+{
+	private:
+			std::string FirstName;
+			std::string LastName;
+			std::string Nickname;
+			std::string PhoneNumber;
+			std::string DarkestSecret;
+
+	public:
+			Contact();
+			~Contact();
+			void	SetFirstName(std::string);
+			void	SetLastName(std::string);
+			void	SetNickname(std::string);
+			void	SetPhoneNumber(std::string);
+			void	SetDarkestSecret(std::string);
+
+			std::string GetFirstName();
+			std::string GetLastName();
+			std::string GetNickname();
+			std::string GetPhoneNumber();
+			std::string GetDarkestSecret();
+};
