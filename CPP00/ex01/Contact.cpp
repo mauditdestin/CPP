adac1e3844d76c987860073850c83c86db5d5a99
paul <akapaui@protonmail.com>
2025-03-05 00:21:26 +0100

    + | CPP00 ex01 almost done.

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -10,3 +10,62 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+Contact::Contact()
+{
+}
+
+Contact::~Contact()
+{
+}
+
+void	Contact::SetFirstName(std::string Firstname)
+{
+	this->FirstName = FirstName;
+}
+
+void	Contact::SetLastName(std::string LastName)
+{
+	this->LastName = LastName;
+}
+
+void	Contact::SetNickname(std::string Nickname)
+{
+	this->Nickname = Nickname;
+}
+
+void	Contact::SetPhoneNumber(std::string PhoneNumber)
+{
+	this->PhoneNumber = PhoneNumber;
+}
+
+void	Contact::SetDarkestSecret(std::string DarkestSecret)
+{
+	this->DarkestSecret = DarkestSecret;
+}
+
+std::string		Contact::GetFirstName()
+{
+	return this->FirstName;
+}
+
+std::string		Contact::GetLastName()
+{
+	return this->LastName;
+}
+
+std::string		Contact::GetPhoneNumber()
+{
+	return this->PhoneNumber;
+}
+
+std::string		Contact::GetNickname()
+{
+	return this->Nickname;
+}
+
+std::string		Contact::GetDarkestSecret()
+{
+	return this->DarkestSecret;
+}
--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -10,3 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Contact.hpp"
+
+class PhoneBook
+{
+	private:
+			int	ContactsNumber;
+			Contact Contacts[8];
+	public:
+			PhoneBook();
+			~PhoneBook();
+			void	SetContactsNumber(int);
+			int		GetContactsNumber();
+			void	EnterNewContact(Contact);
+			Contact GetContactByIndex(int index);
+};
