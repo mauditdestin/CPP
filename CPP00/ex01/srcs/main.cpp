732734a7c95e4420c1e3e26e653daf1865ae18dc
catlover4242 <akapaui@protonmail.com>
2025-05-10 01:16:59 +0200

    + | Rebuild from 0

---
+++ CPP00/ex01/srcs/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleblond <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 01:12:41 by pleblond          #+#    #+#             */
+/*   Updated: 2025/05/10 01:12:41 by pleblond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/PhoneBook.hpp"
+#include <iostream>
+
+void instructions(void)
+{
+	std::cout << "Welcome to the PhoneBook" << std::endl;
+	std::cout << "Please ADD or SEARCH for a contact" << std::endl;
+	std::cout << "You can elso EXIT (all data will be lost)" << std::endl;
+}
+
+int main(void)
+{
+	std::string input;
+	int index = 42;
+	instructions();
+	while ()
+	return (0);
+}
