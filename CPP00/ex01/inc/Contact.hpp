4c7a05f6f2c2c9ab41d2fcc49b1292cc4d38846a
catlover4242 <akapaui@protonmail.com>
2025-05-11 14:00:34 +0200

    + | oupsi j'ai oublie un : partout

--- CPP00/ex01/inc/Contact.hpp
+++ CPP00/ex01/inc/Contact.hpp
@@ -25,17 +25,17 @@ class Contact{
 			void	set_secret(std::string& secret);
 			void	set_index(int newindex);
 
-			std:string get_first_name();
-			std:string get_last_name();
-			std:string get_nickname();
-			std:string get_phone_numb();
-			std:string get_secret();
-			std:string get_index();
+			std::string get_first_name();
+			std::string get_last_name();
+			std::string get_nickname();
+			std::string get_phone_numb();
+			std::string get_secret();
+			std::string get_index();
 			int index;
 	private:
-			std:string first_name();
-			std:string last_name();
-			std:string nickname();
-			std:string phone_number();
-			std:string secret();
+			std::string first_name();
+			std::string last_name();
+			std::string nickname();
+			std::string phone_number();
+			std::string secret();
 };
