7fc744f87d57e548ced18bdb2199b86f2da9c1f5
catlover4242 <akapaui@protonmail.com>
2025-07-08 22:47:40 +0200

    + | Finished randomChump

--- CPP01/ex00/src/randomChump.cpp
+++ CPP01/ex00/src/randomChump.cpp
@@ -10,3 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Zombie.hpp"
+
+void randomChump(std::string name)
+{
+	Zombie zombie(name);
+	zombie.announce();
+}
