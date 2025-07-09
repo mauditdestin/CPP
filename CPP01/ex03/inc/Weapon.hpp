8039ee84b1f687cd9a6bbd606433cfa8f3c708dc
catlover4242 <akapaui@protonmail.com>
2025-07-09 11:49:46 +0200

    + | Did weapon class

--- CPP01/ex03/inc/Weapon.hpp
+++ CPP01/ex03/inc/Weapon.hpp
@@ -10,3 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+
+class Weapon
+{
+	public:
+		Weapon(std::string type);
+		~Weapon();
+		std::string getType() const;
+		void setType(std::string newType);
+
+	private:
+		std::string	type;
+};
--- CPP01/ex03/src/main.cpp
+++ CPP01/ex03/src/main.cpp
@@ -10,3 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/HumanA.hpp"
+#include "../inc/HumanB.hpp"
+#include "../inc/Weapon.hpp"
+#include <cstdlib>
+
+int main()
+{
+	return EXIT_SUCCESS;
+}
