047458ede8c7b849e623f624a552a7f4c94d29fa
catlover4242 <akapaui@protonmail.com>
2025-07-09 12:40:09 +0200

    + | Did makefile

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -16,11 +16,15 @@ void replace(std::string *line, std::string to_replace, std::string new_str)
 {
 	size_t start_pos;
 
-	while ((start_pos = (*line).find(to_replace)))
+	while ((start_pos = (*line).find(to_replace)) != std::string::npos)
+	{
+		(*line).erase(start_pos, to_replace.length());
+		(*line).insert(start_pos, new_str);
+	}
 }
 
 int main(int agrc, char **argv)
 {
-	
+
 	return EXIT_SUCCESS;
 }
