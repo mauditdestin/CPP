d5e787d9662f366d8d8cde4e6110ab5a4d95563d
catlover4242 <akapaui@protonmail.com>
2025-07-09 12:38:39 +0200

    + | Started ex04 and doing geat!

---
+++ CPP01/ex04/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleblond <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 12:34:49 by pleblond          #+#    #+#             */
+/*   Updated: 2025/07/09 12:34:49 by pleblond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/main.hpp"
+
+void replace(std::string *line, std::string to_replace, std::string new_str)
+{
+	size_t start_pos;
+
+	while ((start_pos = (*line).find(to_replace)))
+}
+
+int main(int agrc, char **argv)
+{
+	
+	return EXIT_SUCCESS;
+}
