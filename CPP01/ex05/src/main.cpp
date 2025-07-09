66a56142c316f0b66d6d7e2877c56196758f8dff
catlover4242 <akapaui@protonmail.com>
2025-07-10 00:09:49 +0200

    + | ex05 finished

--- CPP01/ex05/src/main.cpp
+++ CPP01/ex05/src/main.cpp
@@ -10,3 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Harl.hpp"
+
+int main()
+{
+	Harl _Harl;
+	_Harl.complain("debug");
+	_Harl.complain("not a message");
+	_Harl.complain("info");
+	_Harl.complain("warning");
+	_Harl.complain("error");
+	return 0;
+}
