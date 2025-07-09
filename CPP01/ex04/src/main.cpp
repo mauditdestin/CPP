1b5548a90ffbe7531250e547add7951ba9f6f6dc
catlover4242 <akapaui@protonmail.com>
2025-07-09 12:49:36 +0200

    + | Finished ex04

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -28,8 +28,40 @@ void replace(std::string *line, const std::string &to_replace, const std::string
 
 }
 
-int main(int agrc, char **argv)
+int main(int argc, char **argv)
 {
+	if (argc != 4 || !argv[2][0] || !argv[3][0])
+	{
+		std::cout << "Arguments error." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::ifstream input(argv[1]);
+	if (!input.is_open())
+	{
+		std::cout << "Could not open input file." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string name = std::string(argv[1]) + ".replace";
+	std::ofstream output(name.c_str());
+	if (!output.is_open())
+	{
+		std::cout << "Could not open output file." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string line;
+	std::string to_replace = argv[2];
+	std::string new_str (argv[3]);
+
+	while (std::getline(input, line))
+	{
+		replace(&line, to_replace, new_str);
+		output << line << std::endl;
+	}
+	
+	input.close();
 	output.close();
 	return EXIT_SUCCESS;
 }
