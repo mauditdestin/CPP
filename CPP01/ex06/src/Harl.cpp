8ac36a91ed9747bf3d279501081286fbae55aee5
catlover4242 <akapaui@protonmail.com>
2025-07-10 15:44:18 +0200

    + | Finished Harl.cpp

--- CPP01/ex06/src/Harl.cpp
+++ CPP01/ex06/src/Harl.cpp
@@ -41,3 +41,39 @@ void Harl::error()
 {
 	std::cout << "i wanna speak to the manager" << std::endl;
 }
+
+void	Harl::complain(std::string arg)
+{
+	MemberFunction tab[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string msg[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int index = -1;
+
+	int i = 0;
+	while (i < 4)
+	{
+		if (arg == msg[i])
+		{
+			index = i;
+			break;
+		}
+	}
+
+	switch (index)
+	{
+		case 0:
+		case 1:
+		case 2:
+		case 3: {
+			int j = index;
+			while (j < 4)
+			{
+				(this->*tab[j])();
+				j++;
+			}
+			break;
+		}
+		default:
+			std::cout << "probably complaining about something insignificant" << std::endl;
+			break;
+	}
+}
