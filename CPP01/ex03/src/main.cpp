974e43c441bb9a4e0ad663b804cbfafc965c5445
catlover4242 <akapaui@protonmail.com>
2025-07-10 11:13:02 +0200

    + | Added Weapon and main.cpp

--- CPP01/ex03/src/main.cpp
+++ CPP01/ex03/src/main.cpp
@@ -17,5 +17,17 @@
 
 int main()
 {
+	Weapon club = Weapon("crude spike club");
+	HumanA bob("bob", &club);
+	bob.attack();
+	club.setType("some other type of club");
+	bob.attack();
+	HumanB jim("jim");
+	jim.setWeapon(club);
+	jim.attack();
+	club.setType("pickaxe");
+	jim.attack();
+	HumanA marc("marc", NULL);
+	marc.attack();
 	return EXIT_SUCCESS;
 }
