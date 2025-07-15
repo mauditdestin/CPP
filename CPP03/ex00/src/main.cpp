e9bb33c4454f40431039130430cdabc38a8f7e93
mauditdestin <akapaui@protonmail.com>
2025-07-16 00:47:14 +0200

    + | Added main.cpp

--- CPP03/ex00/src/main.cpp
+++ CPP03/ex00/src/main.cpp
@@ -11,3 +11,46 @@
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+
+int main(void)
+{
+    #if PRETTY
+    	std::cout << "╔═══════════════╦════════════╦════════════════════════════════╦════════════╗" << std::endl;
+		std::cout << "║     Event     ║    Name    ║           Description          ║  Success?  ║" << std::endl;
+		std::cout << "╠═══════════════╬════════════╬════════════════════════════════╬════════════╣" << std::endl;
+    #endif
+    {
+        ClapTrap clap1("Zoe");
+        ClapTrap clap2("Paul");
+        {
+            ClapTrap clap3(clap1);
+            ClapTrap clap4;
+            clap4 = clap2;        
+        }
+        clap1.beRepaired(10);
+        clap1.beRepaired(79);
+        for (size_t i = 0l; i < 5; i++)
+        {
+            clap1.attack("Paul");
+        }
+        clap1.takeDamage(100);
+        clap1.attack("Paul");
+        for (size_t i = 0l; i < 5; i++)
+        {
+            clap2.attack("Zoe");
+        }
+        clap1.beRepaired(100);
+        clap1.beRepaired(100);
+        clap2.attack("Zoe");
+        clap1.attack("Paul");
+    }
+    {
+        ClapTrap clap1("Zoe");
+        clap1.takeDamage(8000);
+        clap1.attack("Someone");
+        clap1.beRepaired(100);
+    }
+    #if PRETTY
+        std::cout << "╚═══════════════╩════════════╩════════════════════════════════╩════════════╝" << std::endl;
+    #endif
+}
