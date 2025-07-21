3063b37d3ba7be78f31a942c5630507d3803b76f
mauditdestin <akapaui@protonmail.com>
2025-07-21 23:41:12 +0200

    + | Header for Wrongcat and cpp

---
+++ CPP04/ex00/src/WrongCat.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 23:38:21 by marvin            #+#    #+#             */
+/*   Updated: 2025/07/21 23:38:21 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/WrongCat.hpp"
+
+WrongCat::WrongCat()
+{
+    std::cout << "WrongCat default constructor called" << std::endl;
+    WrongAnimal::_type = "WrongCat";
+}
