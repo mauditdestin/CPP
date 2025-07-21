dfbea8a76a47f5656792256d5a4b1599ecde8b1e
mauditdestin <akapaui@protonmail.com>
2025-07-21 23:39:29 +0200

    + | Modified files, added wronganimal cpp + hpp

--- CPP04/ex00/inc/WrongAnimal.hpp
+++ CPP04/ex00/inc/WrongAnimal.hpp
@@ -1,13 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Peon.cpp                                           :+:      :+:    :+:   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/21 18:34:07 by marvin            #+#    #+#             */
-/*   Updated: 2025/07/21 18:34:07 by marvin           ###   ########.fr       */
+/*   Created: 2025/07/21 23:37:51 by marvin            #+#    #+#             */
+/*   Updated: 2025/07/21 23:37:51 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Peon.hpp"
+#pragma once
+#include <iostream>
+
+class WrongAnimal
+{
+	protected:
+		std::string _type;
+	public:
+		WrongAnimal();
+		WrongAnimal(std::string name);
+		WrongAnimal(WrongAnimal &copy);
+		virtual ~WrongAnimal();
+		WrongAnimal & operator = ( WrongAnimal const & value );
+		std::string getType() const;
+		void		setType(std::string type);
+		void makeSound() const;
+};
