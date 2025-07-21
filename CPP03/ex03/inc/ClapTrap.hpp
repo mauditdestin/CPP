f30596077a1b33b8887cd5d69857bbe08785ce4d
mauditdestin <akapaui@protonmail.com>
2025-07-21 18:26:49 +0200

    + | Added ClapTrap.hpp

---
+++ CPP03/ex03/inc/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 18:08:35 by marvin            #+#    #+#             */
+/*   Updated: 2025/07/16 18:08:35 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <limits.h>
+
+#ifndef PRETTY
+# define PRETTY false
+#endif
+
+class ClapTrap
+{
+	protected:
+		std::string _name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+	public:
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap &ref);
+		~ClapTrap(void);
+		ClapTrap &operator=(ClapTrap &ref);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
