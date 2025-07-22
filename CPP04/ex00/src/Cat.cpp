8209b481a93540571ce98e55a96bc02f05a46330
mauditdestin <akapaui@protonmail.com>
2025-07-22 21:16:49 +0200

    + | Added cat cpp

---
+++ CPP04/ex00/src/Cat.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 23:38:05 by marvin            #+#    #+#             */
+/*   Updated: 2025/07/21 23:38:05 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Cat.hpp"
+
+Cat::Cat()
+{
+    std::cout << "Cat default constructor called" << std::endl;
+    Animal::_type = "Cat";
+}
+
+Cat::~Cat()
+{
+    std::cout << "Cat destructor called" << std::endl;
+}
+
+Cat::Cat(Cat &copy)
+{
+    std::cout << "Cat copy constructor called" << std::endl;
+    *this = copy;
+}
+
+void Cat::makeSound(void) const
+{
+    std::cout << "Meow!" << std::endl;
+}
