b1fb93d25e8feb0f73077418bb01704e763f0704
mauditdestin <akapaui@protonmail.com>
2025-07-22 21:20:49 +0200

    + | Added dog cpp

--- CPP04/ex00/src/Dog.cpp
+++ CPP04/ex00/src/Dog.cpp
@@ -10,3 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Dog.hpp"
+
+Dog::Dog()
+{
+    std::cout << "Dog default constructor called" << std::endl;
+    Animal::_type = "Dog";
+}
+
+Dog::Dog(Dog &copy)
+{
+    std::cout << "Dog copy constructor called" << std::endl;
+    this->_type = copy.getType();
+}
+
+Dog::~Dog()
+{
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+Dog & Dog::operator=(Dog const &value)
+{
+    std::cout << "Dog assignment operator called" << std::endl;
+    if (this != &value)
+    {
+        this->_type = value.getType();
+    }
+    return *this;
+}
+
+void Dog::makeSound(void) const
+{
+    std::cout << "Woof!" << std::endl;
+}
