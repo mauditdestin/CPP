bd41bb4b2b4798fa4d1d8a106bc8a1e2084af73a
mauditdestin <akapaui@protonmail.com>
2025-07-24 17:00:48 +0200

    + | Added Brain.hpp

--- CPP04/ex01/inc/Brain.hpp
+++ CPP04/ex01/inc/Brain.hpp
@@ -11,3 +11,21 @@
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+#include <string>
+#include "Animal.hpp"
+
+class Brain
+{
+    private:
+        std::string _ideas[100];
+    public:
+        Brain();
+        Brain(Brain &copy);
+        ~Brain();
+		Brain & operator = ( Brain const & value );
+        std::string getIdeas(int i) const;
+        void setIdeas(std::string content, int i);
+
+};
--- CPP04/ex01/src/Brain.cpp
+++ CPP04/ex01/src/Brain.cpp
@@ -12,3 +12,36 @@
 
 #include "../inc/Brain.hpp"
 
+Brain::Brain()
+{
+    std::cout << "Brain default constructor called" << std::endl;
+}
+
+Brain::Brain(Brain &copy)
+{
+    std::cout << "Brain copy constructor called" << std::endl;
+    *this = copy;
+}
+
+Brain & Brain::operator=( Brain const & value )
+{
+	std::cout << "Copy assignement operator called" << std::endl;
+    for (int i = 0; i < 99; i++)
+        this->_ideas[i] = value.getIdeas(i);
+    return (*this);
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain destructor called" << std::endl;
+}
+
+std::string Brain::getIdeas(int i) const
+{
+    return (this->_ideas[i % 100]);
+}
+
+void    Brain::setIdeas(std::string content, int i)
+{
+    this->_ideas[i % 100] = content;
+}
