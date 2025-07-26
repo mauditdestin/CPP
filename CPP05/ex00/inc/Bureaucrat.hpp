77b63255ff46ef989a89397c48693423929c9009
mauditdestin <akapaui@protonmail.com>
2025-07-26 23:24:33 +0200

    + | Finished class

--- CPP05/ex00/inc/Bureaucrat.hpp
+++ CPP05/ex00/inc/Bureaucrat.hpp
@@ -5,9 +5,26 @@
 #include <exception>
 
 class Bureaucrat {
-    protected:
+    private:
         Bureaucrat(void);
+        const std::string _name;
+        unsigned short int _grade;
     public:
-        Bureaucrat(std::string const &name, int grade);
+        Bureaucrat(std::string const name, unsigned short int const grade);
         Bureaucrat(Bureaucrat const &src);
+        virtual ~Bureaucrat(void);
+        Bureaucrat &operator=(Bureaucrat const &rhs);
+        void IncGrade(void);
+        void DecGrade(void);
+        class GradeTooLowException : public std::exception {
+                virtual const char *what() const throw();
+        };
+        class GradeTooHighException : public std::exception {
+                virtual const char *what() const throw();
+        };
+        std::string const getName(void) const;
+        int getGrade(void) const;
+
 };
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &i);
