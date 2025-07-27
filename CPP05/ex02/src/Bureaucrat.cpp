a2a41bf17e6553d1b81a98567fd7704db1473019
mauditdestin <akapaui@protonmail.com>
2025-07-27 17:24:00 +0200

    + | Added files for ex01

---
+++ CPP05/ex02/src/Bureaucrat.cpp
@@ -0,0 +1,101 @@
+#include "../inc/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+	this->_grade = 0;
+}
+
+Bureaucrat::Bureaucrat(std::string name)
+{
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+	this->_name = name;
+	this->_grade = 0;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat &copy)
+{
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Default destructor called" << std::endl;
+}
+
+int Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+std::string Bureaucrat::getName(void) const
+{
+	return(this->_name);
+}
+
+Bureaucrat & Bureaucrat::operator=( Bureaucrat const & value )
+{
+	std::cout << "Copy assignement operator called" << std::endl;
+	this->_grade = value.getGrade();
+	this->_name = value.getName();
+	return (*this);
+}
+
+
+void Bureaucrat::setGrade(int grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->_grade = grade;	
+}
+
+void	Bureaucrat::incrementGrade(void)
+{
+	this->_grade -= 1;
+	if (_grade < 1)
+	{
+		_grade++;
+		throw Bureaucrat::GradeTooHighException();
+	}
+}
+
+void	Bureaucrat::decrementGrade(void)
+{
+	this->_grade += 1;
+	if (_grade > 150)
+	{
+		_grade--;
+		throw Bureaucrat::GradeTooLowException();
+	}
+}
+
+
+void Bureaucrat::signForm(Form & Form)
+{
+	if (Form.getGradeExec() < 1 || Form.getGradeExec() > 150)
+		throw SignException();
+	else if (Form.getGradeSign() < 1 || Form.getGradeSign() > 150)
+		throw SignException();
+	else if (Form.getSignStatus() == true)
+		std::cout << this->_name << " signed " << Form.getName() << std::endl;
+	else if (Form.getGradeSign() > this->_grade)
+	{
+		Form.setSigned(true);
+		std::cout << this->_name << " signed " << Form.getName() << std::endl;
+	}
+	else
+	{
+		std::cout << "Grade : " << Form.getGradeSign() << std::endl;
+		throw SignException();
+	}
+}
+
+std::ostream & operator<<( std::ostream & out, Bureaucrat const & Bureaucrat)
+{
+	out << Bureaucrat.getName() << ", bureaucrat grade " << Bureaucrat.getGrade();
+	return (out);
+}
---
+++ CPP05/ex02/src/main.cpp
@@ -0,0 +1,61 @@
+#include "../inc/Bureaucrat.hpp"
+
+int main(void)
+{
+	std::cout << "=================================" << std::endl;
+	std::cout << "\033[1;31m Creating a Bureaucrat\033[m" << std::endl;
+	std::cout << "=================================" << std::endl;
+	Bureaucrat Br("Smith");
+	Form test("Petition", false, 42, 48);
+	
+	std::cout << "=================================" << std::endl;
+	std::cout << "\033[1;32m Changing his grade\033[m" << std::endl;
+	std::cout << "=================================" << std::endl;
+	try
+	{
+		std::cout << Br << std::endl;
+		Br.setGrade(41);
+		std::cout << test << std::endl;
+		Br.signForm(test);
+		std::cout << test << std::endl;
+		Br.setGrade(145);
+		std::cout << Br << std::endl;
+		Br.setGrade(2);
+		std::cout << Br << std::endl;
+		test.setSigned(false);
+		Br.signForm(test);
+		Br.setGrade(65);
+		std::cout << Br << std::endl;
+		
+		std::cout << "=================================" << std::endl;
+		std::cout << "\033[1;33m incrementing his grade\033[m" << std::endl;
+		std::cout << "=================================" << std::endl;
+		Br.incrementGrade();
+		
+		std::cout << Br << std::endl;
+		std::cout << "=================================" << std::endl;
+		std::cout << "\033[1;34m incrementing his grade\033[m" << std::endl;
+		std::cout << "=================================" << std::endl;
+		
+		Br.decrementGrade();
+		std::cout << Br << std::endl;
+		std::cout << "=================================" << std::endl;
+		std::cout << "\033[1;35m Trying to sign with inssufisent grade\033[m" << std::endl;
+		std::cout << "=================================" << std::endl;
+		
+		Br.setGrade(82);
+		test.setSigned(false);
+		Br.signForm(test); // <- exception because grade is insuffisent
+	}
+	catch (std::exception const &e)
+	{
+		std::cout << "=================================" << std::endl;
+		std::cout << "\033[1;31m EXCEPTION CATCHED\033[m" << std::endl;
+		std::cout << "=================================" << std::endl;
+		std::cout << Br.getName() << " ";
+		std::cerr << e.what() << std::endl;
+		std::cout << "=================================" << std::endl;
+		std::cout << "\033[1;31m EXCEPTION CATCHED\033[m" << std::endl;
+		std::cout << "=================================" << std::endl;
+	}
+}
